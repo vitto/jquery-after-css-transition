e79a6d9ff03474eb3284fc5b8506a16134fff9a8
Vittorio Vittori <me@vittoriovittori.com>
2014-01-02 14:38:51 +0100

    Bug fix of transition event which was firing twice in some cases

--- jquery.after-css-transition-1.0.0.js
+++ jquery.after-css-transition-1.0.0.js
@@ -2,24 +2,29 @@ var transitionEnd = "webkitTransitionEnd otransitionend oTransitionEnd msTransit
 
 $.fn.addClassAfter = function(className) {
 	var $this = this;
+	this.unbind(transitionEnd);
 	setTimeout(function(){
 		$this.addClass(className);
 	},1);
 };
 $.fn.addClassAfterTransition = function(className) {
-	this.bind(transitionEnd, function() {
+	this.unbind(transitionEnd);
+	this.bind(transitionEnd, function(e) {
 		$(this).addClass(className);
 		$(this).unbind(transitionEnd);
+
 	});
 	return this;
 };
 $.fn.removeClassAfter = function(className) {
 	var $this = this;
+	this.unbind(transitionEnd);
 	setTimeout(function(){
 		$this.removeClass(className);
 	},1);
 };
 $.fn.removeClassAfterTransition = function(className) {
+	this.unbind(transitionEnd);
 	this.bind(transitionEnd, function() {
 		$(this).removeClass(className);
 		$(this).unbind(transitionEnd);
